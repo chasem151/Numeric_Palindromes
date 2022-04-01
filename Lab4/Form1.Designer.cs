3588272a6323d53532d1cc3b95c82c874dd89fac
unknown <maivaldc2@gmail.com>
2022-04-01 12:42:54 -0400

    Everything working

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find Numeric Palindromes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "by Chase Maivald";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(578, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(276, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter a starting integer (0-1,000,000,000):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Enter count (0-100):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 263);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(100, 158);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.WordWrap = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 424);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(284, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Please enter a positive integer within range.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "EC447 Lab 4";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private static string ReverseStr(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        private static string OneDigit(int num)
+        {
+            int max = 11;
+            if(num < 10)
+            {
+                return num.ToString();
+            }
+            else
+            {
+                return max.ToString();
+            }
+        }
+        
+        private static Boolean TwoPlusDigits(int num)
+        {
+            string number = num.ToString();
+            string reverse = String.Empty;
+            reverse = ReverseStr(number);
+            if (reverse == number)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Hide();
+            string num = textBox1.Text; // starting num, is string
+            string countstr = textBox2.Text; // count of palindromes, is string
+
+            int number = Int32.Parse(num); // convert starting number to integer, check range
+            int count = Int32.Parse(countstr); // convert palindromes count to integer, check range
+
+            int n = 0; // check closeness to max palindrome count
+            Boolean twoplus = false; // check if 2+ digit number is valid
+
+            if (count > 100 || count < 0) // if count out of bounds
+            {
+                textBox3.Text = String.Empty;
+                label5.Show();  
+            }
+            if (number > 1000000000 || number < 0) // if number is out of bounds
+            {
+                textBox3.Text = String.Empty;
+                label5.Show();
+            }
+            else if (count >= 0 && count <= 100 && number >= 0 && number <= 1000000000) // if all valid
+            {
+                textBox3.Text = String.Empty;
+
+                while (n < count)
+                {
+                    string update = String.Empty;
+                    if(number <= 10)
+                    {
+                        update = OneDigit(number);
+                        textBox3.AppendText(update);
+                        textBox3.AppendText(Environment.NewLine);
+                        if(Int32.Parse(update) == 11)
+                        {
+                            number = 11;
+                        }
+                        number += 1;
+                        n += 1;
+                    }
+                    else
+                    {
+                        twoplus = TwoPlusDigits(number);
+                        if(twoplus == true)
+                        {
+                            textBox3.AppendText(number.ToString());
+                            textBox3.AppendText(Environment.NewLine);
+                            number += 1;
+                            n += 1;
+                        }
+                        else if(twoplus == false)
+                        {
+                            number += 1;
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.Handled=!char.IsDigit(e.KeyChar))
+            {
+                textBox3.Text = String.Empty;
+                label5.Show();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.Handled = !char.IsDigit(e.KeyChar))
+            {
+                textBox3.Text = String.Empty;
+                label5.Show();
+            }
+        }
+    }
+}
